fd7afc0ede8e07a817886ab9bbb5a92fe4c20ea5
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-06-03 23:05:52 -0400

    socket() creation

--- CSocketChatlog/server.c
+++ CSocketChatlog/server.c
@@ -6,7 +6,17 @@
 
 
 int main(int argc, char *argv[]) {
-         
+    
+    
+    //IPv4, TCP, system chooses protocol
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //socket() returns a unsigned int so if its value is -1 it failed
+    if (server_fd == "INVALID_SOCKET") {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
 
     return 0;
 }
