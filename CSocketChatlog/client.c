26b14e01219add208f843c37af12b66e614c92c0
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-06-04 21:36:41 -0400

    creation of winsock struct wsa

--- CSocketChatlog/client.c
+++ CSocketChatlog/client.c
@@ -4,7 +4,12 @@
 #include <winsock2.h>
 
 int main(int argc, char *argv[]) {
-    
+
+    WSADATA wsa;
+    if(WSAStartup(MAKEWORD(2,2), &wsa != 0)) {
+         printf("WSAStartup failed. Error code: %d\n", WSAGetLastError());
+        return 1;
+    }
 
     return 0;
 }
