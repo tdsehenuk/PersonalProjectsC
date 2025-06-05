8d119d60660c31dc92a4498404927887897a343c
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-06-04 22:19:13 -0400

    added loop for user to send multiple msgs

--- CSocketChatlog/client.c
+++ CSocketChatlog/client.c
@@ -6,14 +6,14 @@
 int main(int argc, char *argv[]) {
 
     WSADATA wsa;
-    if(WSAStartup(MAKEWORD(2,2), &wsa != 0)) {
-         printf("WSAStartup failed. Error code: %d\n", WSAGetLastError());
+    if(WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+         printf("\nWSAStartup failed. Error code: %d\n", WSAGetLastError());
         return 1;
     }
 
     SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(client_socket == INVALID_SOCKET) {
-        printf("Socket Creation failed. Error code %s", WSAGetLastError());
+        printf("\nSocket Creation failed. Error code %d", WSAGetLastError());
         WSACleanup();
         return 1;
     }
@@ -25,35 +25,43 @@ int main(int argc, char *argv[]) {
 
 
     if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("There was an error connecting to the server. Error code %s", WSAGetLastError());
+        printf("\nThere was an error connecting to the server. Error code %d", WSAGetLastError());
         closesocket(client_socket);
         return 1;
     } 
 
-    printf("Connected to server!\n");
+    printf("\nConnected to server!\n");
 
-    const char *msg = "Hello from client";
-    int bytes_sent = send(client_socket, msg, (int)(strlen(msg)), 0 );
-    
-    if(bytes_sent == SOCKET_ERROR) {
-        printf("Send failed. Error code %s" , WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-    }
+    //const char *msg = "\nHello from client";
+  while (1) {
+        char msg[512];
+        printf("Enter message (type 'exit' to quit): ");
+        fgets(msg, sizeof(msg), stdin);
+        msg[strcspn(msg, "\n")] = 0;  // remove newline
 
-    printf("Message was sent to the server");
+        if (strcmp(msg, "exit") == 0)
+            break;
 
-    char buffer[512];
-    int bytes_recieved = recv(client_socket, buffer, sizeof(buffer) -1 , 0);
-    if(bytes_recieved == SOCKET_ERROR) {
-        printf("Recieved failed. Error code %d", WSAGetLastError);
-    } else if(bytes_recieved == 0) {
-        printf("Connection was closed by the server. Error code %s", WSAGetLastError());
-    } else {
-        buffer[bytes_recieved] = '\0';
-        printf("server replied: %s", buffer);
-    }
+        int bytes_sent = send(client_socket, msg, (int)strlen(msg), 0);
+        if (bytes_sent == SOCKET_ERROR) {
+            printf("Send failed. Error code %d\n", WSAGetLastError());
+            break;
+        }
+
+        char buffer[512];
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received == SOCKET_ERROR) {
+            printf("Receive failed. Error code %d\n", WSAGetLastError());
+            break;
+        } else if (bytes_received == 0) {
+            printf("Server closed the connection.\n");
+            break;
+        } else {
+            buffer[bytes_received] = '\0';
+            printf("Server replied: %s\n", buffer);
+        }
+}
+
 
     closesocket(client_socket);
     WSACleanup();
